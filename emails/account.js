24d89ae257a9930dcae60887d82bd20274afb741
anumolugoutham <anumolu.goutham@gmail.com>
2019-07-27 11:02:31 +0530

    =absja

--- emails/account.js
+++ emails/account.js
@@ -3,7 +3,7 @@ const smail=require("@sendgrid/mail")
 // const r=require("../public/index.html")
 
 // console.log(username)
-smail.setApiKey("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")
+smail.setApiKey("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")
 
 const welcomemail=function(email,name,x){
 smail.send({
